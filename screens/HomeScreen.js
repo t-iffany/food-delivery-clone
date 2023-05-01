32af7d96838542f1bf9f1aea090e72a1d9e87ad5
t-iffany <tiffanyxleong@gmail.com>
2023-05-01 16:55:07 -0700

    Use useNavigation and useLayoutEffect to hide default header. Use SafeAreaView to move content to viewable area.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, SafeAreaView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
+
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="text-red-500">HomeScreen</Text>
+    </SafeAreaView>
   )
 }
 
