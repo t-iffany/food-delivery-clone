42f2108ca8a41b8922ce029d34b0e9302ba86798
t-iffany <tiffanyxleong@gmail.com>
2023-05-01 17:19:24 -0700

    Create homescreen Header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,12 @@
-import { Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
+import { 
+  ChevronDownIcon,
+  UserIcon, 
+  SearchIcon,
+  AdjustmentsIcon,
+} from 'react-native-heroicons/outline';
 
 const HomeScreen = () => {
 
@@ -13,8 +19,27 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      <Text className="text-red-500">HomeScreen</Text>
+    <SafeAreaView className="bg-white pt-5">
+      <Text className="text-red-500">
+        {/* header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image 
+            source={{
+              uri: "https://links.papareact.com/wru"
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+            <Text className="font-bold text-xl">Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+
+          <UserIcon size={35} color="#00CCBB" />
+        </View>
+      </Text>
     </SafeAreaView>
   )
 }
