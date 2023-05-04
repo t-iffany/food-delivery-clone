5e691d5546d671fcc30e2465e02d4ba8232c720a
t-iffany <tiffanyxleong@gmail.com>
2023-05-03 20:14:10 -0700

    Add search bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,11 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { 
   ChevronDownIcon,
   UserIcon, 
-  SearchIcon,
-  AdjustmentsIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
 } from 'react-native-heroicons/outline';
 
 const HomeScreen = () => {
@@ -20,9 +20,9 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white pt-5">
-      <Text className="text-red-500">
+      {/* <Text className="text-red-500"> */}
         {/* header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
           <Image 
             source={{
               uri: "https://links.papareact.com/wru"
@@ -31,17 +31,33 @@ const HomeScreen = () => {
           />
 
           <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-            <Text className="font-bold text-xl">Current Location
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
               <ChevronDownIcon size={20} color="#00CCBB" />
             </Text>
           </View>
 
           <UserIcon size={35} color="#00CCBB" />
         </View>
-      </Text>
+
+        {/* search bar */}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+            <MagnifyingGlassIcon color="gray" size={20} />
+            <TextInput 
+              placeholder="Restaurants and cuisines" 
+              keyboardType="default"
+            />
+          </View>
+
+          <AdjustmentsVerticalIcon color="#00CCBB" />
+        </View>
+      {/* </Text> */}
     </SafeAreaView>
   )
 }
 
-export default HomeScreen
+export default HomeScreen;
