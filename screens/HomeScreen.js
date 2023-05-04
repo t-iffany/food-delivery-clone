b765080b958ab755e533ae67261faf08f622a0b1
t-iffany <tiffanyxleong@gmail.com>
2023-05-03 21:19:53 -0700

    Add Categories component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { 
@@ -7,6 +7,7 @@ import {
   MagnifyingGlassIcon,
   AdjustmentsVerticalIcon,
 } from 'react-native-heroicons/outline';
+import Categories from '../components/categories';
 
 const HomeScreen = () => {
 
@@ -56,6 +57,13 @@ const HomeScreen = () => {
           <AdjustmentsVerticalIcon color="#00CCBB" />
         </View>
       {/* </Text> */}
+      {/* body */}
+      <ScrollView className="bg-gray-100">
+        {/* categories section with slider */}
+        <Categories />
+
+        {/* featured rows */}
+      </ScrollView>
     </SafeAreaView>
   )
 }
