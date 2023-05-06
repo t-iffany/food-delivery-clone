0bbf21887566734717f9ac5d7247f0f7458c7e3f
t-iffany <tiffanyxleong@gmail.com>
2023-05-05 21:06:57 -0700

    Style titles, descriptions. Add ArrowRight. Add ScrollView to FeaturedRows.

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native';
+import React from 'react';
+import { ArrowRightIcon } from 'react-native-heroicons/outline';
 
 const FeaturedRow = ({ id, title, description }) => {
   return (
     <View>
-      <Text>FeaturedRow</Text>
-    </View>
-  )
-}
+      <View className="mt-4 flex-row items-center justify-between px-4">
+        <Text className="font-bold text-lg">{title}</Text>
+        <ArrowRightIcon color="#00CCBB" />
+      </View>
 
-export default FeaturedRow
+      <Text className="text-xs text-gray-500 px-4">{description}</Text>
+
+      <ScrollView
+        horizontal
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+        }}
+        showsHorizontalScrollIndicator={false}
+        className="pt-4"
+      >
+        {/* restaurant cards */}
+
+      </ScrollView>
+    </View>
+  );
+};
+
+export default FeaturedRow;
